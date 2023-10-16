db944c94ffcdcbae50a97a086f6da850bae5e648
Alexander Frank <alexander.frank@onecode.de>
2023-10-16 09:21:49 +0100

    Add cors header to server response

--- server/src/main/java/com/lexfury/kkon/PoiRoute.kt
+++ server/src/main/java/com/lexfury/kkon/PoiRoute.kt
@@ -2,6 +2,7 @@ package com.lexfury.kkon
 
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
+import io.ktor.server.response.header
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
@@ -9,6 +10,7 @@ import io.ktor.server.routing.routing
 fun Application.poiRoute(pointOfInterestRepository: PointOfInterestRepository) {
 	routing {
 		get("/pois") {
+			call.response.header("Access-Control-Allow-Origin", "*")
 			call.respond(pointOfInterestRepository.getPointsOfInterest())
 		}
 	}
