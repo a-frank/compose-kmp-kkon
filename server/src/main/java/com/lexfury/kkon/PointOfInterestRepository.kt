a6910242623704427742dc2ef3953a63c7547f69
Alexander Frank <alexander.frank@onecode.de>
2023-10-16 08:59:03 +0100

    Update dto data

--- server/src/main/java/com/lexfury/kkon/PointOfInterestRepository.kt
+++ server/src/main/java/com/lexfury/kkon/PointOfInterestRepository.kt
@@ -14,25 +14,25 @@ private val poiResponse: PointOfInterestResponse = PointOfInterestResponse(
 			"1",
 			coordinates = LatLongDto(52.5200, 13.4050),
 			"Berlin",
-			"Hauptstadt"
+			"Hauptstadt, Reichstag, Fernsehturm, Alexanderplatz, Brandenburger Tor"
 		),
 		PointOfInterestDto(
 			"2",
 			coordinates = LatLongDto(50.1109, 8.6821),
 			"Frankfurt",
-			"Mainhatten"
+			"Mainhatten, Börse, Banken, Skyline"
 		),
 		PointOfInterestDto(
 			"3",
 			coordinates = LatLongDto(49.0069, 8.4037),
 			"Karlsruhe",
-			"Bundesverfassungsgericht"
+			"Bundesverfassungsgericht, Schloss, ZKM"
 		),
 		PointOfInterestDto(
 			"4",
 			coordinates = LatLongDto(53.5488, 9.9872),
 			"Hamburg",
-			"Fischmarkt"
+			"Hafen, Fischmarkt, Elbphilharmonie"
 		),
 	)
 )
